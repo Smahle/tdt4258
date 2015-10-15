035d2d7dd6be731c83729bcb08c0e151d24e10cf
kjerstto <kjersti.tobro@gmail.com>
2015-10-15 11:31:21 +0200

    Update interrupt_handlers.c

--- ex2_support/interrupt_handlers.c
+++ ex2_support/interrupt_handlers.c
@@ -22,7 +22,7 @@ void __attribute__ ((interrupt)) TIMER1_IRQHandler()
   uint_fast8_t buttons = (*GPIO_PC_DIN & 0xFF);
   *GPIO_PA_DOUT = buttons << 8;
 
-enableSleep();
+
   double angular_frequency;
   if (buttons == 0xFE){ /*check which button is being pressed*/
     if (t<=(SAMPLING_FREQ)){
